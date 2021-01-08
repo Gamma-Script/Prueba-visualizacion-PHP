df47ebcf75c5b8aea5c2f3dcbab4103643d7f6ea
Andres Henriquez <hg19010@ues.edu.sv>
2021-01-08 14:04:11 -0600

    se añadieron headers al file-write y un nuevo archivo php

--- file-write.php
+++ file-write.php
@@ -1,6 +1,8 @@
 <?php
-    $fp = fopen("code-editable.php", "w") or die("Unable to open file!");
+    header('Access-Control-Allow-Origin: *');
+
     $editorCode = $_POST['input'];
+    $fp = fopen("code-editable.php", "w") or die("Unable to open file!");
     fwrite($fp, $editorCode);
     fclose($fp);
 ?>
---
+++ lanzador.php
@@ -0,0 +1,4 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+include('code-editable.php');
+?>
